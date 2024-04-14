d64b5af246bb23b520be3b7fb15234e79cc5fd12
Nicholas Blott <blottn@tcd.ie>
2024-04-14 19:22:41 +0200

    Refactor + beautification of style

--- ui.js
+++ ui.js
@@ -2,7 +2,20 @@ import { el, mount, unmount, list, setAttr, setChildren } from "redom";
 import { id } from './id.js';
 import { mutate, getState } from './state.js';
 
-const mount_point = document.body.querySelector("#grims");
+
+export const refresh = () => {
+  console.log('refresh ui', getState());
+  let {meta, grims} = getState();
+
+  refreshGrims();
+  refreshST();
+  refreshControls();
+};
+
+export const refreshControls = () => {
+
+};
+
 
 const st_who = document.body.querySelector("#st-who");
 const st_become = document.body.querySelector("#st-become");
@@ -13,13 +26,18 @@ st_become.onclick = () => {
     return s;
   });
 };
+
 export const refreshST = () => {
   let {meta} = getState();
   st_who.innerText = meta.st ?? 'unselected';
 }
 
+const mount_point = document.body.querySelector("#grims");
+
 export const refreshGrims = () => {
   let {meta, grims} = getState();
+  console.log('grims refresh');
+  console.log(grims);
   setChildren(mount_point,
     [el('div', Object.entries(grims ?? {'a': 'foo'}).map(([p_id, a]) => {
       return el('p', JSON.stringify(a));
@@ -27,11 +45,4 @@ export const refreshGrims = () => {
   );
 }
 
-export const refresh = () => {
-  console.log('refresh ui', getState());
-  let {meta, grims} = getState();
-
-  refreshGrims();
-  refreshST()
-};
 
