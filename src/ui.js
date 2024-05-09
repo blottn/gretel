fbd72d9db68787e0da396baf49d28b471d2cef25
Nicholas Blott <blottn@tcd.ie>
2024-05-09 13:01:28 +0200

    Added initial icons + styling on untaken seats

--- src/ui.js
+++ src/ui.js
@@ -55,8 +55,14 @@ export const refreshGrims = () => {
 export const render_grim = (p_id, grim) => {
   const { seats } = getState();
   return el('div',
+    el('p', `${p_id}`),
     Object.entries(grim).map(([seat_id, {token, reminders}]) => {
-      return el('div', el('button', token, {class:'token'}), el('p', seats[seat_id].owner), el('p', JSON.stringify(reminders)));
+      return el('div',
+        el('img', token, {class: 'token', src: "https://raw.githubusercontent.com/bra1n/townsquare/d9c2b17dc9b2d7b091b55c988707784f12980f44/src/assets/icons/amnesiac.png"}),
+        el('p', seats[seat_id].owner, 
+          {class: `seat ${seats[seat_id].owner}`}),
+        Object.keys(reminders).map(r => el('p', JSON.stringify(r)))
+      );
     })
   );
 }
