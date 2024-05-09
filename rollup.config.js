e4baaff255be05d4c82358f0ed4941eca0737bb6
Nicholas Blott <blottn@tcd.ie>
2024-05-09 11:11:24 +0200

    Move under src/ directory

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'gretel.js',
+  input: 'src/gretel.js',
   output: {
     file: 'dist/bundle.js',
     format: 'es'
